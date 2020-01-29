8b0adc9551fa82c459fd35b5540bb0df0b7953dd
Fredika Putra <fredikaputra6@gmail.com>
2020-01-30 06:48:51 +0800

    Add style

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+
+		<title>Mail Dashboard</title>
+
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+
+		<header>
+
+			<div class="left">
+				<a href="">MAIL</a>
+				<a href=""><i class="fas fa-bars"></i></a>
+				<form class="search">
+					<input type="search"><button type="submit" name="button"><i class="fas fa-search"></i></button>
+				</form>
+			</div>
+
+			<div class="right">
+				<i class="fas fa-bell"></i>
+				<i class="fas fa-user"></i>
+				<span>Amelia</span>
+				<i class="fas fa-caret-down"></i>
+			</div>
+
+		</header>
+
+		<main>
+
+		</main>
+
+		<footer>
+
+		</footer>
+
+	</body>
+
+	<script src="https://kit.fontawesome.com/3e2bdce50a.js"></script>
+
+</html>
