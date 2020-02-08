29b7d6e548edb3265320b9948fd01e31a672ef5c
Fredika Putra <fredikaputra6@gmail.com>
2020-02-08 14:32:21 +0800

    Episode 3 MVC

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+
+class About{
+	public function index($nama = 'Fredika', $pekerjaan = 'Siswa'){
+		echo "Halo nama saya $nama, saya seorang $pekerjaan ";
+	}
+	
+	public function page(){
+		echo "about/page";
+	}
+}
